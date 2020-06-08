bc94ef7c202dd6a65590a154fac16f558b19ccbf
eduardt04 <turtoieduard@yahoo.com>
2020-06-08 10:35:09 +0300

    lab3-finished

--- lab3.sql
+++ lab3.sql
@@ -78,11 +78,133 @@ SELECT
 FROM employees emps
     RIGHT JOIN departments deps ON (deps.department_id = emps.department_id)
 ORDER BY 2;
+
     
--- 9 --
-SELECT 
-    emps.first_name "Nume",
-    deps.department_id "Cod departament"
-FROM employees emps
-    LEFT JOIN departments deps ON (emps.department_id = deps.department_id);
-    
+-- 10 --
+SELECT department_id 
+FROM departments
+WHERE UPPER(department_name) LIKE '%RE%'
+UNION
+SELECT department_id
+FROM employees
+WHERE UPPER(job_id) LIKE '%SA_REP%';
+
+-- 12 --
+SELECT DISTINCT department_id
+FROM departments
+MINUS
+SELECT DISTINCT department_id
+FROM employees;
+
+SELECT department_id
+FROM departments
+WHERE ( SELECT COUNT(employee_id) 
+        FROM employees
+        WHERE employees.department_id = departments.department_id ) = 0;
+        
+-- 13 --
+SELECT department_id
+FROM departments
+WHERE UPPER(department_name) LIKE '%RE%'
+INTERSECT
+SELECT department_id
+FROM employees
+WHERE UPPER(job_id) LIKE '%HR_REP%';
+
+-- 14 --
+SELECT employee_id "Cod angajat", 
+       job_id "Cod job", 
+       last_name "Nume"
+FROM employees
+WHERE salary > 3000
+UNION
+SELECT employee_id "Cod angajat", 
+       job_id "Cod job", 
+       last_name "Nume"
+FROM employees JOIN jobs USING(job_id)
+WHERE employees.salary = (jobs.min_salary + jobs.max_salary) / 2;
+
+-- 15 --
+SELECT last_name "Nume",
+       hire_date "Data"
+FROM employees
+WHERE hire_date > ( SELECT hire_date
+                    FROM employees
+                    WHERE UPPER(last_name) LIKE '%GATES%');
+
+-- 16 --
+SELECT last_name "Nume",
+       salary "Salariu"
+FROM employees
+WHERE department_id IN (SELECT department_id
+                        FROM employees
+                        WHERE UPPER(last_name) LIKE '%GATES%'); 
+
+-- 17 --
+SELECT last_name "Nume",
+       salary "Salariu"
+FROM employees
+WHERE manager_id = (SELECT employee_id
+                    FROM employees
+                    WHERE manager_id is NULL);
+
+-- 18 --
+SELECT last_name "Nume",
+       department_id "Cod departament",
+       salary "Salariu"
+FROM employees
+WHERE (department_id, salary) IN (SELECT department_id, salary
+                                  FROM employees
+                                  WHERE commission_pct IS NOT NULL);
+
+-- 19 --
+SELECT employee_id "Cod",
+       last_name "Nume",
+       salary "Salariu"
+FROM employees
+JOIN jobs USING (job_id)
+WHERE salary > (min_salary + max_salary)/2 AND
+department_id IN (SELECT department_id
+                  FROM employees
+                  WHERE UPPER(last_name) LIKE '%T%');
+                  
+-- 20 --
+SELECT last_name "Nume",
+       department_id "Cod departament",
+       salary "Salariu"
+FROM employees
+WHERE salary >ALL (SELECT salary
+                   FROM employees
+                   WHERE UPPER(job_id) LIKE '%CLERK%'); 
+
+-- 21 --
+SELECT emps.last_name "Nume",
+       deps.department_name "Nume departament",
+       emps.salary "Salariu"
+FROM employees emps JOIN departments deps USING (department_id)
+WHERE emps.commission_pct IS NULL
+AND emps.manager_id IN (SELECT emps2.manager_id 
+                        FROM employees emps2
+                        WHERE emps2.commission_pct IS NOT NULL);
+                        
+-- 22 --
+SELECT last_name "Nume",
+       department_id "Departament",
+       salary "Salariu"
+FROM employees
+WHERE (salary, commission_pct) IN (SELECT salary, commission_pct
+                                   FROM employees 
+                                   JOIN departments USING (department_id)
+                                   JOIN locations USING(location_id)
+                                   WHERE UPPER(city) LIKE '%OXFORD%');
+
+-- 23 --
+SELECT last_name "Nume",
+       department_id "Cod departament",
+       job_id "Cod job"
+FROM employees
+WHERE department_id IN (SELECT department_id 
+                        FROM departments JOIN locations USING (location_id)
+                        WHERE UPPER(city) LIKE '%TORONTO%');
+                        
+                                   
